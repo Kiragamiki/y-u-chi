c85710e6b27cacc0b27aa3c76378e8645bbcfcad
Kiragamiki <104133775+Kiragamiki@users.noreply.github.com>
2022-04-22 14:59:08 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Chi à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi này :))
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'phải chăng anh đã yêu từ cái nhìn đầu tiên',
+    desc: 'làm người yêu anh nhá :> ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà :). Yêu em nhiều nhiều :)))',
+    messDesc: 'yêu công chúa.',
+    btnAccept: 'Okiiiii lun <333',
+    messLink: 'https://www.facebook.com/profile.php?id=100054771392926' //link mess của các bạn https://www.messenger.com/t/100002861780475/
+}
